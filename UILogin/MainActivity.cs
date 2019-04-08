0764c1a12255ad669195d09f283139157310241d
Nishank <nishank@thumbmunkeys.com>
2019-04-08 16:25:11 +0530

    Other learning projects removed and other things added

--- UILogin/MainActivity.cs
+++ UILogin/MainActivity.cs
@@ -11,16 +11,23 @@ namespace UILogin
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private EditText emailEditText;
+        private EditText passwordEditText;
+        private Button loginButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            Button loginButton = FindViewById<Button>(Resource.Id.LoginButton);
+            loginButton = FindViewById<Button>(Resource.Id.LoginButton);
+            emailEditText = FindViewById<EditText>(Resource.Id.EmailInput);
+            passwordEditText = FindViewById<EditText>(Resource.Id.PasswordInput);
             
             loginButton.Click += LoginButton_Click;
         }
+
 
         protected override void OnResume()
         {
@@ -41,10 +48,29 @@ namespace UILogin
             Log.Info("OnDestroy","On Destroy from main activity");
         }
 
+        protected bool validateEmail()
+        {
+           bool validEmail = true;
+           string email = emailEditText.Text.ToString();
+
+           Log.Info("Validate email", email);
+           return validEmail;
+        }
+
+        protected bool validatePassword()
+        {
+            string password = passwordEditText.Text.ToString();
+
+            return (password.Length > 6);
+        }
+
         private void LoginButton_Click(object sender, System.EventArgs e)
         {
-            var intent = new Intent(this,typeof(BackActivity));
-            StartActivity(intent);
+            if (validateEmail() && validatePassword())
+            {
+                var intent = new Intent(this, typeof(BackActivity));
+                StartActivity(intent);
+            }
         }
 
     }
