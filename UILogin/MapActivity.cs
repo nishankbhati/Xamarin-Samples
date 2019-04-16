34d27902fea68c453e925206016fd889770460d8
Nishank <nishank@thumbmunkeys.com>
2019-04-16 11:30:05 +0530

    Google map inntegration into the app

---
+++ UILogin/MapActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace UILogin
+{
+    [Activity(Label = "MapActivity")]
+    public class MapActivity : Activity, IOnMapReadyCallback
+    {
+        private GoogleMap mMap;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.activity_maps);
+            SetUpMap();
+        }
+
+        private void SetUpMap()
+        {
+            if (mMap == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.Gmap).GetMapAsync(this);
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            mMap = googleMap;
+        }
+    }
+}
